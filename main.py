e885c55d6f4145ca5b872364d32f09640139b9d2
Jonny Le <jonny.le@computacenter.com>
2021-12-06 19:52:10 +0000

    bug: catch unsupported access methods

--- main.py
+++ main.py
@@ -90,6 +90,9 @@ def main():
                         logger.warning(f'IP Address cannot be found for {ci["name"]}.')
                         ci_result['host'] = ci['ip_address']
                         ci_result['status'] = 'Could not find hostname or IP address.'
+            else:
+                logger.warning(f'Do not recognize methods \'{ci["u_primary_acces_method"]}\' or \'{ci["u_secondary_access_method"]} ')
+                ci_result['status'] = f'Primary method \'{ci["u_primary_acces_method"]}\' and secondary method \'{ci["u_secondary_access_method"]} are not supported.'
         except Exception as e:
             logger.exception(f'Uncaught exception: {e}')
             ci_result['status'] = f'Unknown error: {e}'
